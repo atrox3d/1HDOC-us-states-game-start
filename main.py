2cbd89f0f30cff23609737e5b7d8af7d0d18abb3
atrox3d <atrox3d@gmail.com>
2021-02-16 07:08:58 +0100

    add comments

--- main.py
+++ main.py
@@ -22,41 +22,64 @@ screen.bgpic(IMAGE)
 #
 states = pandas.read_csv("50_states.csv")
 print(states)
-#
+#########################################################################################################
 #   https://towardsdatascience.com/filtering-data-frames-in-pandas-b570b1f834b9
 #   how does boolean filtering work
-#
-#
+#########################################################################################################
 print(states['state'] == 'Texas')       # returns a data series of booleans
 print(states['x'] > 200)                # returns a data series of booleans
 print(states[states.state == 'Texas'])  # the data series of booleans is passed as argument
-
-
+#########################################################################################################
+#
+#   setup variables
+#
 gameover = False
 guesses = []
 score = 0
+#
+#   main loop
+#
 while not gameover:
+    #
+    # get answer Title case
+    #
     answer = screen.textinput(title=f"{score}/{len(states)} States Correct", prompt="What's another state's name?").title()
+    #
+    # failsafe
+    #
     if answer == "Stop":
         gameover = True
         break
     print(f"answer: {answer}")
+    #
+    # check if answer is correct
+    #
     check = states[states.state == answer]
     print(f"check: {check}")
     print(f"len check: {len(check)}")
     if len(check) != 0:
+        #
+        #   if correct create new object to write state on map
+        #
         state = State(
             check.state.values[0],
             check.x.values[0],
             check.y.values[0],
         )
         state.show()
+        #
+        #   save answer/state object
+        #
         guesses.append(state)
+        #
+        #   update score
+        #
         score += 1
         if score == 50:
+            #
+            #   win
+            #
             gameover = True
-
-
 
 
 # screen.mainloop()
